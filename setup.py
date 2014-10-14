04880b23528aa8140491baadf718ec87e3c88545
claeyswo <wouter.claeys@rwo.vlaanderen.be>
2014-10-14 14:05:32 +0200

    Tag 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = [
 
 setup(
     name='skosprovider_rdf',
-    version='0.1.3',
+    version='0.2.0',
     description='skosprovider_rdf',
     long_description=README + '\n\n' + CHANGES,
     package_data={'': ['LICENSE']},

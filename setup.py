9d258e16389a124b76e54980205c61498c1aeae5
Wouter Claeys <wouter.claeys@rwo.vlaanderen.be>
2014-10-14 14:10:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ except ImportError:
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.rst')) as f:
-    CHANGES = f.read()
+with open(os.path.join(here, 'HISTORY.rst')) as f:
+    HISTORY = f.read()
 
 packages = [
     'skosprovider_rdf',

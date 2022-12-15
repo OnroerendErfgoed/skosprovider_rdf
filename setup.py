9a68fb35e971caac8d7df45e6371f3132a85e9f2
Koen Van Daele <koen_van_daele@telenet.be>
2022-12-15 12:11:25 +0100

    Basisconfig updaten.

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     author='Flanders Heritage Agency',
     author_email='ict@onroerenderfgoed.be',

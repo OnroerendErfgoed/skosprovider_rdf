3f0de7307781f5dc112dac69e4613657c3e19925
Tim Van Campenhout <45393594+vancamti@users.noreply.github.com>
2026-04-14 15:30:22 +0200

    Use proper linting tools (#136)
    * Use proper linting tools
    
    Introduce ruff with pre-commit configuration.
    
    Fixes #133
    
    * review fix

--- examples/load_skos.py
+++ examples/load_skos.py
@@ -6,31 +6,30 @@ from skosprovider_rdf.providers import RDFProvider
 
 graph = Graph()
 
-file = os.path.join(os.path.dirname(__file__), '..', 'tests', 'data', 'simple_turtle_products')
-graph.parse(file, format="turtle")
-
-provider = RDFProvider(
-    {'id': 'PRODUCTS'},
-    graph
+file = os.path.join(
+    os.path.dirname(__file__), '..', 'tests', 'data', 'simple_turtle_products'
 )
+graph.parse(file, format='turtle')
 
-print("provider.get_all()")
-print("------------------")
+provider = RDFProvider({'id': 'PRODUCTS'}, graph)
+
+print('provider.get_all()')
+print('------------------')
 print(provider.get_all())
-print("")
+print('')
 
 print("provider.find({'label': 'jewelry'})")
-print("-----------------------------------")
+print('-----------------------------------')
 print(provider.find({'label': 'jewelry'}))
-print("")
+print('')
 
 
 print("provider.get_by_id('http://wwww.products.com/Jewellery')")
-print("--------------------------------------------------------")
+print('--------------------------------------------------------')
 print(provider.get_by_id('http://www.products.com/Jewellery'))
-print("")
+print('')
 
 print("provider.get_by_uri('http://wwww.products.com/Jewellery')")
-print("---------------------------------------------------------")
+print('---------------------------------------------------------')
 print(provider.get_by_uri('http://www.products.com/Jewellery'))
-print("")
+print('')
